e4de85f293cf52f36fac92a2aa9d27b7713ea365
Michael G. Noll <mnoll@verisign.com>
2014-03-13 18:38:39 +0100

    Enhance unit test suite

--- spec/defines/kafka_broker_spec.rb
+++ spec/defines/kafka_broker_spec.rb
@@ -20,8 +20,18 @@ describe 'kafka::broker' do
           it { should contain_class("kafka::params") }
           it { should contain_class("kafka") }
 
-          it { should contain_file('/opt/kafka/config/server-0.properties').
-            with_content(/^log.dirs=\/app\/kafka-broker-0/)}
+          it { should contain_file('/opt/kafka/config/server-0.properties')
+            .with_content(/^broker.id=0$/)
+            .with_content(/^port=9092$/)
+            .with_content(/^log.dirs=\/app\/kafka-broker-0$/)
+            .with_content(/^zookeeper.connect=localhost:2181$/)
+          }
+          it { should contain_file('/opt/kafka/config/log4j-0.properties')
+            .with_content(/^log4j.appender.kafkaAppender.File=\/var\/log\/kafka\/server-0.log$/)
+            .with_content(/^log4j.appender.stateChangeAppender.File=\/var\/log\/kafka\/state-change-0.log$/)
+            .with_content(/^log4j.appender.requestAppender.File=\/var\/log\/kafka\/kafka-request-0.log$/)
+            .with_content(/^log4j.appender.controllerAppender.File=\/var\/log\/kafka\/controller-0.log$/)
+          }
         end
 
         describe "kafka broker with two directories for $log_dirs on #{osfamily}" do
@@ -29,8 +39,8 @@ describe 'kafka::broker' do
             :log_dirs => ['/app/kafka-broker-0', '/app/kafka-broker-1'],
           }}
 
-          it { should contain_file('/opt/kafka/config/server-0.properties').
-            with_content(/^log.dirs=\/app\/kafka-broker-0,\/app\/kafka-broker-1/)}
+          it { should contain_file('/opt/kafka/config/server-0.properties')
+            .with_content(/^log.dirs=\/app\/kafka-broker-0,\/app\/kafka-broker-1$/)}
         end
 
       end
