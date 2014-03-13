dc7147bc9fc90d307d95cb45d5caa066380de357
Michael G. Noll <mnoll@verisign.com>
2014-03-13 21:57:30 +0100

    Try different line break syntax to be compatible with Ruby 1.8.7

--- spec/defines/kafka_broker_spec.rb
+++ spec/defines/kafka_broker_spec.rb
@@ -23,17 +23,17 @@ describe 'kafka::broker' do
           it { should contain_class("kafka") }
           it { should contain_kafka__broker('broker0') }
 
-          it { should contain_file(default_broker_configuration_file)
-            .with_content(/^broker.id=0$/)
-            .with_content(/^port=9092$/)
-            .with_content(/^log.dirs=\/app\/kafka-broker-0$/)
-            .with_content(/^zookeeper.connect=localhost:2181$/)
+          it { should contain_file(default_broker_configuration_file).
+            with_content(/^broker.id=0$/).
+            with_content(/^port=9092$/).
+            with_content(/^log.dirs=\/app\/kafka-broker-0$/).
+            with_content(/^zookeeper.connect=localhost:2181$/)
           }
-          it { should contain_file(default_logging_configuration_file)
-            .with_content(/^log4j.appender.kafkaAppender.File=\/var\/log\/kafka\/server-0.log$/)
-            .with_content(/^log4j.appender.stateChangeAppender.File=\/var\/log\/kafka\/state-change-0.log$/)
-            .with_content(/^log4j.appender.requestAppender.File=\/var\/log\/kafka\/kafka-request-0.log$/)
-            .with_content(/^log4j.appender.controllerAppender.File=\/var\/log\/kafka\/controller-0.log$/)
+          it { should contain_file(default_logging_configuration_file).
+            with_content(/^log4j.appender.kafkaAppender.File=\/var\/log\/kafka\/server-0.log$/).
+            with_content(/^log4j.appender.stateChangeAppender.File=\/var\/log\/kafka\/state-change-0.log$/).
+            with_content(/^log4j.appender.requestAppender.File=\/var\/log\/kafka\/kafka-request-0.log$/).
+            with_content(/^log4j.appender.controllerAppender.File=\/var\/log\/kafka\/controller-0.log$/)
           }
         end
 
@@ -42,9 +42,7 @@ describe 'kafka::broker' do
             :broker_id => 23,
           }}
 
-          it { should contain_file(default_broker_configuration_file)
-            .with_content(/^broker.id=23$/)
-          }
+          it { should contain_file(default_broker_configuration_file).with_content(/^broker.id=23$/) }
         end
 
         describe "kafka broker with a custom port on #{osfamily}" do
@@ -52,9 +50,7 @@ describe 'kafka::broker' do
             :broker_port => 9093,
           }}
 
-          it { should contain_file(default_broker_configuration_file)
-            .with_content(/^port=9093$/)
-          }
+          it { should contain_file(default_broker_configuration_file).with_content(/^port=9093$/) }
         end
 
         describe "kafka broker with a single custom ZK server for $zookeeper_connect on #{osfamily}" do
@@ -62,8 +58,8 @@ describe 'kafka::broker' do
             :zookeeper_connect => ['zookeeper1:1234'],
           }}
 
-          it { should contain_file(default_broker_configuration_file)
-            .with_content(/^zookeeper.connect=zookeeper1:1234$/)
+          it { should contain_file(default_broker_configuration_file).
+            with_content(/^zookeeper.connect=zookeeper1:1234$/)
           }
         end
 
@@ -72,8 +68,8 @@ describe 'kafka::broker' do
             :zookeeper_connect => ['zookeeper1:1234', 'zookeeper2:5678','zkserver3:2181'],
           }}
 
-          it { should contain_file(default_broker_configuration_file)
-            .with_content(/^zookeeper.connect=zookeeper1:1234,zookeeper2:5678,zkserver3:2181$/)
+          it { should contain_file(default_broker_configuration_file).
+            with_content(/^zookeeper.connect=zookeeper1:1234,zookeeper2:5678,zkserver3:2181$/)
           }
         end
 
@@ -82,8 +78,8 @@ describe 'kafka::broker' do
             :log_dirs => ['/app/kafka-broker-0', '/app/kafka-broker-1'],
           }}
 
-          it { should contain_file(default_broker_configuration_file)
-            .with_content(/^log.dirs=\/app\/kafka-broker-0,\/app\/kafka-broker-1$/)
+          it { should contain_file(default_broker_configuration_file).
+            with_content(/^log.dirs=\/app\/kafka-broker-0,\/app\/kafka-broker-1$/)
           }
         end
 
